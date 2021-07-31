de287a221f18d44da65693cd2584e4073b026a29
rafael <r.amizes@gmail.com>
2021-07-31 19:41:12 -0300

    format example/main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,5 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:well_formed/well_formed.dart';
 
 void main() {
-  runApp(FormFieldTester(BasicTextField(trim: true, blank: 'Please fill in this field')));
+  runApp(
+    FormFieldTester(
+      BasicTextField(trim: true, blank: 'Please fill in this field'),
+    ),
+  );
 }
